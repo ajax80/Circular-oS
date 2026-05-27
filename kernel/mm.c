243f1539d490774886c196bc536c6c9c9b971786
Jonathan Ayers <ayersjon80@gmail.com>
2026-05-26 23:04:39 -0500

    Add AGPL-3.0 license headers to all source files

--- kernel/mm.c
+++ kernel/mm.c
@@ -1,3 +1,26 @@
+/*
+ * Circular OS
+ * Copyright (C) 2026 Jonathan Eugene Ayers <ayersjon80@gmail.com>
+ *
+ * This file is part of Circular OS.
+ *
+ * Circular OS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Circular OS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with Circular OS. If not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * For commercial licensing, see COMMERCIAL_LICENSE in this repository.
+ */
+
 #include "mm.h"
 #include "serial.h"
 
